30fc43b972877702ddee7bb7e2e3b3ad714cddc1
fatin2030 <fatinnoor2030@gmail.com>
2024-06-09 15:21:17 +0600

    Lab Task -01

---
+++ Lab1/Lab1/Models/Education.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1.Models
+{
+    public class Education
+    {
+
+        public string Title {  get; set; }
+        public string Year {  get; set; }
+        public string Group {  get; set; }
+        public string Ins {  get; set; }
+    }
+}
---
+++ Lab1/Lab1/Models/Information.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1.Models
+{
+    public class Information
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Degree { get; set; }
+        public string Expertise { get; set; }
+       
+    }
+}
